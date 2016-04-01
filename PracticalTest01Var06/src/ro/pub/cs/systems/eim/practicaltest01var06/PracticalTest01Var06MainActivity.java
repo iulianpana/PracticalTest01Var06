cfa683eb8771b99d5961a4a23efdf3ba26f806c7
Student User <student@cti.pub.ro>
2016-04-01 16:36:13 +0300

    first commit

--- PracticalTest01Var06/src/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ PracticalTest01Var06/src/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -4,14 +4,87 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 public class PracticalTest01Var06MainActivity extends Activity {
+	private EditText topEditText = null, lowEditText = null;
+	private Button details = null,passButton= null,naviButton = null;
+	private PassListener  passListener = new PassListener();
+	
+	
+	
+	private class ButtonListener implements View.OnClickListener{
 
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			
+			switch (arg0.getId()) {
+			case R.id.details:
+				if (details.getText().toString().contains("More details")) {
+					details.setText("Less details");
+					set
+				}
+				
+				break;
+
+			default:
+				break;
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	private class PassListener implements TextWatcher {
+
+		@Override
+		public void afterTextChanged(Editable arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+			// TODO Auto-generated method stub
+			
+			if(arg0.toString().startsWith("http"))
+				;
+			
+		}}
+	
+	
+	
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_main);
+        
+        topEditText = (EditText)findViewById(R.id.top_edit_text);
+        lowEditText = (EditText)findViewById(R.id.lower_edit_text);
+        details = (Button)findViewById(R.id.details);
+        passButton = (Button)findViewById(R.id.pass_button);
+        naviButton = (Button)findViewById(R.id.navigate_to_secondary_activity);+
+        
+        details.setOnClickListener(l);
+        
+        
     }
 
 
